491734117fe659d403dd387f1b363c90533c94a8
AkaHitsuji <yang025@e.ntu.edu.sg>
2019-07-15 21:38:09 +0800

    readFile function performing correctly

--- fileParserScript.js
+++ fileParserScript.js
@@ -6,4 +6,50 @@ var getCurrWorkingDirectory = function() {
     return currCWD;
 }
 
+var readFilesInDir = function(directory, arrayResults) {
+    fs.readdir(directory, function (err, files) {
+      if (err) return arrayResults(error);
+      var i = 0;
+      (function next() {
+          var file = files[i++];
+          if(!file) return arrayResults(null);
+
+          file = directory + '/' + file;
+          fs.stat(file, function (error, stat) {
+            if (error) {
+              console.error("Error stating file.", error);
+              return;
+            }
+
+            else if (stat && stat.isDirectory()) {
+                readFilesInDir(file, function (error) {
+                    next();
+                });
+            } else {
+                // if file is a file
+                // TODO: readFile function to check if it contains todo
+                arrayResults.push(file)
+                next();
+            }
+        });
+    })();
+});
+}
+
+var containsSearchString = false;
+var checkIfFileContainsString = function(filePath, searchString) {
+    fs.readFile(filePath, "utf8", function (err, data) {
+      if (err) throw err;
+
+      if(data.includes(searchString)){
+          containsSearchString = true;
+      }
+      else containsSearchString = false;
+
+      return containsSearchString;
+  });
+}
+
 module.exports.getCurrWorkingDirectory = getCurrWorkingDirectory;
+module.exports.readFilesInDir = readFilesInDir;
+module.exports.checkIfFileContainsString = checkIfFileContainsString;
--- test/fileParserScriptTest.js
+++ test/fileParserScriptTest.js
@@ -1,12 +1,40 @@
-var fileParser = require('../fileParserScript.js');
+// import {getCurrWorkingDirectory, readFilesInDir, checkIfFileContainsString} from '../fileParserScript';
+// import {expect} from 'chai';
+// import {assert} from 'assert';
+
 var expect = require('chai').expect;
 var assert = require('assert');
+var fileParserScript = require('../fileParserScript.js');
 
-describe('fileParserScript tests', function() {
+describe('fileParserScript tests', () => {
 
-    it('getCurrWorkingDirectory should return cwd', function() {
-        var currDirectory = "/Users/AngYang/gitHub_Projects/FileParserScript"
-        expect(fileParser.getCurrWorkingDirectory()).to.equal(currDirectory)
-    })
+  it('getCurrWorkingDirectory should return cwd', ()=> {
+      const currDirectory = "/Users/AngYang/gitHub_Projects/FileParserScript"
+      expect(fileParserScript.getCurrWorkingDirectory()).to.equal(currDirectory)
+  })
+
+  it('readFilesInDir result should be more than 1', ()=> {
+      var currDirectory = fileParserScript.getCurrWorkingDirectory();
+      var testDirectory = currDirectory + '/test_dir';
+      var arrayResults = [];
+
+      fileParserScript.readFilesInDir(testDirectory, arrayResults, function(err, res) {
+          arrayResults = res;
+          console.log('these are the array results: ',arrayResults);
+          expect(arrayResults.length>1).to.equal(true);
+      })
+  })
+
+  it('readFile should return true for somefile.js', ()=> {
+      var currDirectory = fileParserScript.getCurrWorkingDirectory();
+      var somefileDirectory = currDirectory + "/test_dir/somedir/somemodule/somefile.js"
+
+      var containsTodo;
+
+      fileParserScript.checkIfFileContainsString(somefileDirectory, 'TODO', function(err, res) {
+          containsTodo = res;
+          expect(containsTodo).to.equal(true);
+      })
+  })
 
 })
